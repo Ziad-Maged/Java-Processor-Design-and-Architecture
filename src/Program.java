50eea32c7868e3c9c5e8a7664062ef782f921d46
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-18 18:41:45 +0300

    Initial Commit

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,8 @@
 
 public class Program {
     Instruction[] instructionMemory = new Instruction[1024];
+
+    Data[] dataMemory = new Data[2048];
     public static void main(String[] args) {
 
     }
