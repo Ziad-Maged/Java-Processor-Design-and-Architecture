fb214a217a40458d67e2662e224ce1af8c85da08
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-18 19:29:07 +0300

    Initial Commit

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,34 @@
 
 public class Program {
-    Instruction[] instructionMemory = new Instruction[1024];
-    Data[] dataMemory = new Data[2048];
+    short[] instructionMemory = new short[1024];
+    byte[] dataMemory = new byte[2048];
     byte[] registers = new byte[64];
     static short pc = 0;
     static byte sreg = 0;
+    static int clockCycle = 0;
+
+    public void fetch(){
+        decode(instructionMemory[pc]);
+        clockCycle++;
+        pc++;
+    }
+
+    public void decode(short instruction){
+        String binary = Integer.toBinaryString(instruction);
+        if(binary.length() > 16)
+            binary = binary.substring(16);
+        else if(binary.length() < 16){
+            while(binary.length() < 16)
+                binary = "0" + binary;
+        }
+        byte opcode = Byte.parseByte(binary.substring(0, 4), 2);
+        byte destination = Byte.parseByte(binary.substring(4, 10), 2);
+        byte sourceImmediate = Byte.parseByte(binary.substring(10), 2);
+    }
+
+    public void execute(){
+        //TODO LATER
+    }
 
     public static void main(String[] args) {
         String[] test = Integer.toBinaryString(15).split("");
