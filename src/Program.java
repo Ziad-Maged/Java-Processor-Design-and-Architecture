c2845ff33d4b54e6c319a1c5619f5456217e207d
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-18 17:15:51 +0300

    Initial Commit

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         byte t1 = sc.nextByte();
