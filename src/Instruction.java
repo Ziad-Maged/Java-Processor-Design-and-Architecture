50cc0a159ad2e8543949b15e37a338cea09deba1
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-18 18:39:52 +0300

    Initial Commit

--- src/Instruction.java
+++ src/Instruction.java
@@ -1,6 +1,42 @@
 public class Instruction {
-    short instruction;
-    byte opcode;
-    byte destination;
-    byte sourceImmediate;
+    private short instruction;
+    private byte opcode;
+    private byte destination;
+    private byte sourceImmediate;
+
+    public Instruction(){
+
+    }
+
+    public byte getDestination() {
+        return destination;
+    }
+
+    public byte getOpcode() {
+        return opcode;
+    }
+
+    public short getInstruction() {
+        return instruction;
+    }
+
+    public byte getSourceImmediate() {
+        return sourceImmediate;
+    }
+
+    public void setInstruction(short instruction) {
+        this.instruction = instruction;
+    }
+
+    public void setDestination(byte destination) {
+        this.destination = destination;
+    }
+
+    public void setOpcode(byte opcode) {
+        this.opcode = opcode;
+    }
+
+    public void setSourceImmediate(byte sourceImmediate) {
+        this.sourceImmediate = sourceImmediate;
+    }
 }
