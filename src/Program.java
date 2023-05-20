dee0d5ab3ab377bb95a31acba835762d211cb1b9
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-20 11:59:27 +0300

    Initial Commit

--- src/Program.java
+++ src/Program.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Program {
     static short[] instructionMemory = new short[1024];
