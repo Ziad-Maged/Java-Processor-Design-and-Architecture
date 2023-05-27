b0bf5060efa4836a4b5b8004ec21bd218329fcd8
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-27 14:09:07 +0300

    Initial Commit

---
+++ src/Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+    public Frame(){
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+
+}
