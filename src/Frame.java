23dbb6a1a55df02008d6945960796c3df5b5ebc0
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-27 14:25:18 +0300

    Initial Commit

--- src/Frame.java
+++ src/Frame.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
+
+    JTextArea textField = new JTextArea();
 
     public Frame(){
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Assembly Constructor");
         this.setLocationRelativeTo(null);
+        textField.setBackground(Color.BLACK);
+        textField.setForeground(Color.GREEN);
+        textField.setCaretColor(Color.WHITE);
+        this.add(textField);
         this.setVisible(true);
     }
 
