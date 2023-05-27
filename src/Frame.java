aaf34ccbfa6fc47e646ea3fd5cddc90c895dcbbe
Ziad Elsabbagh <ziad.maged@hotmail.com>
2023-05-27 14:12:41 +0300

    Initial Commit

--- src/Frame.java
+++ src/Frame.java
@@ -5,8 +5,13 @@ public class Frame extends JFrame {
     public Frame(){
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        this.setTitle("Assembly Constructor");
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Frame();
     }
 
 }
